098f3fb482747b7f5c380407f0dea8baecd485d7
OmarRF2304 <147424109+OmarRF2304@users.noreply.github.com>
2023-10-11 17:07:13 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const v1Pokemon = require("./v1/routers/pokemonroutes");
 const v1profesorRouter = require("./v1/routers/profesorRoutes");
 const v1materiaRouter = require("./v1/routers/materiaRoutes");
 const v1Peliculas = require("./v1/routers/peliculasRoutes"); 
+const v1Ip = require("./v1/routers/ipRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ app.use("/api/v1/pokemon", v1Pokemon);
 app.use("/api/v1/profesor", v1profesorRouter);
 app.use("/api/v1/materia", v1materiaRouter);
 app.use("/api/v1/peliculas", v1Peliculas); 
+app.use("/api/v1/ip/", v1Ip);
 
 
 app.listen(PORT, () => {
